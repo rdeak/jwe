f67b9ebd9fca4ad6fcfe33cbdffc3a4eb8c6ecd7
--global <robert.deak@gmail.com>
2024-01-09 13:05:50 +0100

    chore(test): update encrypt test

--- src/encrypt.ts
+++ src/encrypt.ts
@@ -5,19 +5,12 @@ import { compress } from "./compression";
 export async function encrypt(
   payload: Record<string, unknown>,
   secret: string,
-  initializationVector?: string,
 ) {
   const compressedContent = await compress(payload);
 
   let builder = new jose.CompactEncrypt(
     new TextEncoder().encode(compressedContent),
   ).setProtectedHeader({ alg: "dir", enc: "A128GCM" });
-
-  if (initializationVector) {
-    builder = builder.setInitializationVector(
-      Buffer.from(Buffer.from(initializationVector).toString("base64")),
-    );
-  }
 
   return builder.encrypt(await createJWK(secret));
 }
